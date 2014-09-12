95894b7136aefe6e606a36fa5b8508a960bf83a5
Woody Chung <wc9825@gmail.com>
2014-09-11 21:40:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lostinthewoodz::Application.config.secret_key_base = 'b9a84f51bfa8570122383115d481e7135f989643e5563132fc50a9aeb48e6db45b2b6468b1d053d8397a9f9da67e4123791f0db4b6b0f974eb2dc6f8ae08931c'
